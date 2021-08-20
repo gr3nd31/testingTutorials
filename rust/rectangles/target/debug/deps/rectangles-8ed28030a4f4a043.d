503c7e14ef44a8324c73f7ef259a5a00d3f1c4fb
Raisch <grendelsLaw@protonmail.com>
2021-08-19 21:46:54 -0500

    Testing stuff

---
+++ rust/rectangles/target/debug/deps/rectangles-8ed28030a4f4a043.d
@@ -0,0 +1,5 @@
+/home/grendel/Documents/GitHub/testingTutorials/rust/rectangles/target/debug/deps/rectangles-8ed28030a4f4a043: src/main.rs
+
+/home/grendel/Documents/GitHub/testingTutorials/rust/rectangles/target/debug/deps/rectangles-8ed28030a4f4a043.d: src/main.rs
+
+src/main.rs:
---
+++ rust/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/grendel/Documents/GitHub/testingTutorials/rust/rectangles/target/debug/rectangles: /home/grendel/Documents/GitHub/testingTutorials/rust/rectangles/src/main.rs
